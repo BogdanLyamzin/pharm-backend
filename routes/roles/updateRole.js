a6e92f9d357c73279e801bf1d122db7c72a8c69c
ANasypana <alla.nasypana@gmail.com>
2020-03-17 12:01:52 +0200

    Merge branch 'Dennis/Modal' into dennis_1212
    # Conflicts:
    #	dist/css/main.css.map
    #	dist/js/main.js
    #	index.html
    #	src/js/modal.js
    #	src/scss/_header-modal.scss

--- routes/roles/updateRole.js
+++ routes/roles/updateRole.js
@@ -3,15 +3,26 @@ const asyncHandler = require("../../middleware/async");
 const ErrorResponse = require('../../utils/errorResponse');
 
 module.exports = (app) => {
-	app.put("/roles/:id",  asyncHandler(async (req, res) => {
+	app.put("/roles/:id",  asyncHandler(async (req, res, next) => {
 		const id = req.params.id;
+		const qerBody = {...req.body};
 		const role = await Role.findById(id);
+		const pages = [...role.allowPages];
 
 		if (!role) {
 			return next(new ErrorResponse(`Role not found with id of ${id}`, 404));
 		};
 
-		const data = await Role.findByIdAndUpdate(id, req.body);
+		if(qerBody.allowPages && pages.includes(qerBody.allowPages)){
+			return next(new ErrorResponse(`${qerBody.allowPages} is allowed to this role`, 400));
+		}else if(qerBody.allowPages){
+			pages.push(qerBody.allowPages);
+		};
+
+		qerBody.allowPages = pages;
+
+		await Role.findByIdAndUpdate(id, qerBody);
+		const data = await Role.findById(id);
 
 		res.status(201).json({
 			success: true,
