04134a1a2073270b7464cfd3b759382d2b91b9f6
ANasypana <alla.nasypana@gmail.com>
2020-03-18 11:12:49 +0200

    Merge branch 'Dennis/Modal' into dennis_1212
    # Conflicts:
    #	dist/css/main.css.map
    #	dist/js/main.js
    #	index.html
    #	src/js/modal.js
    #	src/scss/_header-modal.scss

--- routes/product/getAllProducts.js
+++ routes/product/getAllProducts.js
@@ -1,11 +1,11 @@
-const AdminUser = require("../../models/AdminUser");
+const Product = require("../../models/Product");
 const Role = require("../../models/Role");
 const advancedResults = require("../../middleware/advancedResults");
 const asyncHandler = require("../../middleware/async")
 
 
 module.exports = (app) => {
-	app.get("/:lan/products", advancedResults(AdminUser), asyncHandler(async (req, res, next) => {
+	app.get("/:lan/products", advancedResults(Product), asyncHandler(async (req, res, next) => {
 
 		res.status(200).json( req.params.lan);
 	}))
