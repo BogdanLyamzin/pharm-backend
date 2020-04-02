96558b52f896ee1f08a36a71ff4b14c6f79af65e
ANasypana <alla.nasypana@gmail.com>
2020-04-02 13:19:50 +0300

    Merge branch 'Dennis/Modal' into dennis_1212
    # Conflicts:
    #	dist/css/main.css.map
    #	dist/js/main.js
    #	index.html
    #	src/js/modal.js
    #	src/scss/_header-modal.scss

--- middleware/error.js
+++ middleware/error.js
@@ -3,8 +3,6 @@ const ErrorResponse = require("../utils/errorResponse");
 const errorHandler = (err, req, res, next) =>{
 	let error = {...err};
 	error.message = err.message;
-	//Log to console for dev
-
 
 	//Mongoose bad ObjectId
 	if(err.name === "CastError"){
